c90e8781be67e952b1f826767f943f911347b1cd
Pawel Borzym <pawel.borzym3095@gmail.com>
2016-04-25 09:05:40 -0700

    code clean

--- ChemistryAssistant/ChemistryAssistant/ViewModel/ReactionViewModel.cs
+++ ChemistryAssistant/ChemistryAssistant/ViewModel/ReactionViewModel.cs
@@ -42,8 +42,7 @@ namespace ChemistryAssistant.ViewModel
                         {
 
                             //populating OC with idreaction and reactionDesc
-                            //reader.GetString("idreaction") + 
-                            // _reactionViewModel._allReactions.Add(new Reaction(reader.GetString("idreaction") + reader.GetString("reactionDesc")));
+                           
                             _reactionViewModel._allReactions.Add(new Reaction(reader.GetString("reactionTitle") + "            " + reader.GetString("reactionDesc")));
                         }
                         MenuPage.chemPageInitialized = true;
@@ -58,31 +57,7 @@ namespace ChemistryAssistant.ViewModel
             return _reactionViewModel.AllReactions;
         }
 
-        //public bool InsertNewTodo(string what)
-        //{
-        //    Todo newTodo = new Todo(what);
-        //    // Insert to the collection and update DB
-        //    try
-        //    {
-        //        using (MySqlConnection connection = new MySqlConnection("Database=ChemDB;Data Source=eu-cdbr-azure-west-d.cloudapp.net;User Id=b1300e7178d3cc;Password=8b9c2183;SslMode=None"))
-        //        {
-        //            connection.Open();
-        //            MySqlCommand insertCommand = connection.CreateCommand();
-        //            insertCommand.CommandText = "INSERT INTO todo(whatToDO)VALUES(@whatToDO)";
-        //            insertCommand.Parameters.AddWithValue("@whatToDO", newTodo.whatToDO);
-        //            insertCommand.ExecuteNonQuery();
-        //            _todoViewModel._allToDos.Add(newTodo);
-        //            return true;
-
-        //        }
-        //    }
-        //    catch(MySqlException)
-        //    {
-        //        // Don't forget to handle it
-        //        return false;
-        //    }
-
-        //}
+       
 
 
         public ReactionViewModel()
